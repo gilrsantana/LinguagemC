6a3bb7ef7a78fb7b742b64ccb28edd150ce811d6
gilrsantana <g.r.santana@hotmail.com>
2020-09-24 16:59:42 -0300

    Atualizaçao de arquivos

--- Projeto_FAC_em_C/Antigos/escapesom.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-        printf("O som /n /a/a/a");
-        return 0;
-}
-
--- Projeto_FAC_em_C/Antigos/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int a){
-    printf("Se a mensagem foi impressa, o programa foi bem executado.\n");
-
-    printf("%d", main(a));
-
-    return 0;
-}
