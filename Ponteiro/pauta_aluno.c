7e3c280115a3e08203ef587123f36271e45667b8
gilrsantana <g.r.santana@hotmail.com>
2020-10-22 21:22:20 -0300

    Created Ponteiro folder and pauta_aluno and valida_bolsa

---
+++ Ponteiro/pauta_aluno.c
@@ -0,0 +1,30 @@
+/*
+ * Para cada aluno de uma turma é lido:
+ * Matrícula, nota1, nota2, nota3 e a frequência 
+ * ( 15 inteiros representando as faltas do aluno nas 15 aulas do curso: 
+ * 0- presente nos dois tempos, 
+ * 1 – presente em apenas 1 tempo ou 
+ * 2- ausente nos dois tempos)
+ * 
+ * Faça um programa, utilizando a função abaixo, que exiba, para cada aluno:
+ * a matrícula, média, presenças, e % de faltas dos alunos 
+ * 
+ * Função  void  processa_pauta( int * numpresencas, float * percfaltas), 
+ * que recebe o endereço de duas variáveis que serão preenchidas com o número de presenças 
+ * e o % de faltas de um aluno calculados a partir da leitura  da freqüência
+ */
+
+
+
+
+#include <stdio.h>
+
+
+int main(void){
+	
+	
+	
+	
+	
+	return 0;
+}
