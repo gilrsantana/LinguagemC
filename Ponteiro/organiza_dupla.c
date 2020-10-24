a9d70a0549fcd731da59b131127a09baa75f2c5f
gilrsantana <g.r.santana@hotmail.com>
2020-10-24 06:35:20 -0300

    Created organiza_dupla.c

---
+++ Ponteiro/organiza_dupla.c
@@ -0,0 +1,31 @@
+/*
+ * Um concurso de duplas sertanejas, realizado em dois dias diferentes,  organiza a apresentação
+ * dos participantes pela diferença entre suas alturas. Faça um programa (usando as funções 
+ * abaixo), que capture o número de cada dupla e  a altura,em cm, de seus 2 integrantes 
+ * informando, para cada dupla: o dia ( 1 ou 2),  o  nº da dupla, maior altura e menor altura de
+ * seus integrantes.
+ * No primeiro dia, ficam as duplas com diferença de altura inferior ou igual a 15 cm e no 
+ * segundo dia as demais duplas. O término do  processamento ocorre quando é informado ‘0’ como 
+ * número da dupla.
+ * 
+ * No final, seu programa deve exibir o nº da dupla com maior diferença e o número da dupla com 
+ * menor diferença (a diferença entre as alturas varia de 0 a 230cm)
+ * 
+ * a)Faça uma função que  coloque nas variáveis cujos endereços são fornecidos na chamada da 
+ * função, o número da dupla e as  duas alturas lidas do teclado. 
+ * Protótipo desta função: void Ler (int * num, int *alt1, int *alt2);
+ * 
+ * b)Faça uma função que  coloque em ordem crescente as alturas armazenadas nas variáveis cujos 
+ * endereços são fornecidos na chamada da função ( na 1ª o mais alto e na segunda o mais baixo) 
+ * e retorne  a diferença entre as duas alturas
+ * Ex: mais alto 190,   mais baixo: 160  diferença: 30cm 
+ * Protótipo da função: int Ordenar (int *alt1,  int *alt2);
+ * 
+ */
+#include <stdio.h>
+
+
+int main(void){
+	
+	return 0;
+}
