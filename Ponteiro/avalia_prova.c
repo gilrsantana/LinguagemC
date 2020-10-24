4135c28c269481d68d970078a5163b5e87895174
gilrsantana <g.r.santana@hotmail.com>
2020-10-24 05:01:29 -0300

    Completion of verifica_departamento and inicialization of avalia_prova program

---
+++ Ponteiro/avalia_prova.c
@@ -0,0 +1,25 @@
+/*
+ * 4.Na correção da prova ou do teste, quando a resposta está certa, o professor coloca 1 
+ * e quando está errada coloca 0. No entanto,  para calcular a nota, cada 3 erros eliminam um 
+ * acerto.
+ * Para cada aluno é introduzido via teclado:
+ * 1- matrícula
+ * 2- suas respostas para as 10 questões da prova ou teste;
+ * 
+ * Faça um programa, utilizando a função contabilizaPontos(...), que calcule e mostre a média
+ * de G1 de cada aluno ( P1*0.8 + T1*0.2), sabendo que todos os alunos compareceram as duas 
+ * avaliações.
+ * 
+ * A função void contabiliza_pontos(int * acertos, int * erros), recebe as variáveis para 
+ * armazenar os acertos e os erros do aluno.  Lê  as  10 respostas do aluno retornando a 
+ * quantidade de acertos e de erros;
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+	
+	
+	return 0;
+}
