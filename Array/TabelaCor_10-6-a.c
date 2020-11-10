2b463db73c3e49f314534ca627303ebb244db8fe
Gilmar Santana <g.r.santana@hotmail.com>
2020-11-10 06:55:57 -0300

    Update TabelaCor including random color

--- Array/TabelaCor_10-6-a.c
+++ Array/TabelaCor_10-6-a.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define AJUSTE(a) ( ((a) * 3) - 3 )
+#define ALUNOS 60
 
 void preencheArray(int referencia, int array[]);
 
@@ -8,17 +11,16 @@ void preencheArray(int referencia, int array[]);
 int main(void){
 	int cor;
 	int palheta[24] = {-1};
-	int matricula;
+	int matricula = 1;
+	
+	srand(time(NULL));
 		
-	printf("Informe o número da matrícula ou 0 para sair: ");
-	scanf("%i", &matricula);
-	while(matricula > 0){
-		printf("Informe o número da cor (1 a 8): ");
-		scanf("%i", &cor);
+	while(matricula <= ALUNOS){
+		cor = (rand() % 8) + 1; 
 		preencheArray(cor, palheta);
-		printf("R = %i / G = %i / B = %i \n\n", palheta[AJUSTE(cor)], palheta[AJUSTE(cor)+1], palheta[AJUSTE(cor)+2]);
-		printf("Informe o número da matrícula ou 0 para sair: ");
-		scanf("%i", &matricula);
+		printf("MATRÍCULA %i\n", matricula);
+		printf("cor %i ... R = %i / G = %i / B = %i \n\n", cor, palheta[AJUSTE(cor)], palheta[AJUSTE(cor)+1], palheta[AJUSTE(cor)+2]);
+		matricula++;
 	}
 	
 	return 0;
