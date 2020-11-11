2151b9df032066a158ec75a8fcb0c058588d1684
Gilmar Santana <g.r.santana@hotmail.com>
2020-11-11 10:53:42 -0300

    Conclusion of file BilheteAviao_10-8-b.c and creation of file DataMatricula_10-9.c

--- Array/BilheteAviao_10-8-b.c
+++ Array/BilheteAviao_10-8-b.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
-#include <locale.h>
 
 #define SIZE 8
 
 void preencheArray(int codigo[], int assento[], float valor[]);
-void compraBilhete(int array1[], float array2[], int ref);
+void compraBilhete(int array1[], float array2[], int ref, int codigo[]);
 int avaliaTarifa(int array[]);
 int verificaPosicao(int codigo[], int ref);
 
 int main(void){
     /*
-     * CÓDIGO 17 - Posição 0
-     * CÓDIGO 48 - Posição 1
-     * CÓDIGO 10 - Posição 2
-     * CÓDIGO 21 - Posição 3
-     * CÓDIGO 96 - Posição 4
-     * CÓDIGO 55 - Posição 5
-     * CÓDIGO 23 - Posição 6
-     * CÓDIGO 16 - Posição 7
+     * CÃ“DIGO 17 - PosiÃ§Ã£o 0
+     * CÃ“DIGO 48 - PosiÃ§Ã£o 1
+     * CÃ“DIGO 10 - PosiÃ§Ã£o 2
+     * CÃ“DIGO 21 - PosiÃ§Ã£o 3
+     * CÃ“DIGO 96 - PosiÃ§Ã£o 4
+     * CÃ“DIGO 55 - PosiÃ§Ã£o 5
+     * CÃ“DIGO 23 - PosiÃ§Ã£o 6
+     * CÃ“DIGO 16 - PosiÃ§Ã£o 7
      */
-    int input;
     int tarifa;
     int bancoTarifa[SIZE] = {17,48,10,21,96,55,23,16};
     int assento[SIZE];
     float valor[SIZE];
 
-    setlocale(LC_ALL,"");
-
     preencheArray(bancoTarifa, assento, valor);
     tarifa = avaliaTarifa(bancoTarifa);
+    
     while(tarifa != 0){
-        if(tarifa > 0){
-            compraBilhete(assento, valor, tarifa);
-        }
-        tarifa = avaliaTarifa(bancoTarifa);
+        if(tarifa == -1){
+			tarifa = avaliaTarifa(bancoTarifa);
+		}else{
+			compraBilhete(assento, valor, tarifa, bancoTarifa);
+			tarifa = avaliaTarifa(bancoTarifa);
+		}
     }
 
     return 0;
@@ -44,21 +43,21 @@ void preencheArray(int codigo[], int assento[], float valor[]){
     for(int i = 0; i < SIZE; i++){
         printf("Informe o valor da tarifa %i: ", codigo[i]);
         scanf("%f", &valor[i]);
-        printf("Informe a quantidade disponível da tarifa %i: ", codigo[i]);
+        printf("Informe a quantidade disponÃ­vel da tarifa %i: ", codigo[i]);
         scanf("%i", &assento[i]);
     }
     return;
 }
 
 ///////////////////////////////////////////////////////////
-void compraBilhete(int array1[], float array2[], int ref){
+void compraBilhete(int array1[], float array2[], int ref, int codigo[]){
     int qtd;
     int pos;
     float compra;
 
-    pos = verificaPosicao(array1, ref);
+    pos = verificaPosicao(codigo, ref);
     if(array1[pos] == 0){
-        printf("\nNão há assentos disponíveis\n");
+        printf("\nNÃ£o hÃ¡ assentos disponÃ­veis\n");
     }else{
         printf("\nInforme a quantidade de bilhetes desejada: ");
         scanf("%i", &qtd);
@@ -68,8 +67,8 @@ void compraBilhete(int array1[], float array2[], int ref){
             array1[pos]-=qtd;
             printf("Valor da compra ... R$ %.2f\n\n", compra);
         }else{
-            printf("Compra não efetuada.\n");
-            printf("Quantidade disponível é de %i assentos.\n", array1[pos]);
+            printf("Compra nÃ£o efetuada.\n");
+            printf("Quantidade disponÃ­vel Ã© de %i assentos.\n", array1[pos]);
         }
     }
     return;
@@ -77,22 +76,26 @@ void compraBilhete(int array1[], float array2[], int ref){
 
 ///////////////////////////////////////////////////////////
 int avaliaTarifa(int array[]){
-    int saida = 0;
     int input;
-    printf("\nInforme o código da tarifa desejada ou 0 para sair: ");
+    printf("\nInforme o cÃ³digo da tarifa desejada ou 0 para sair: ");
     scanf("%i", &input);
-    for(int i = 0; i < SIZE; i++){
-        if(input == array[i]){
-            saida = input;
-        }
-    }
-    return saida;
+    if(input == 0){
+		return 0;
+	}else{
+		for(int i = 0; i < SIZE; i++){
+			if(input == array[i]){
+				return input;
+			}
+		}
+	}
+    return -1;
 }
 ///////////////////////////////////////////////////////////
 int verificaPosicao(int codigo[], int ref){
     for(int i = 0; i < SIZE; i++){
-        if(ref == codigo[i]){
-            return i;
-        }
-    }
+		if(ref == codigo[i]){
+			return i;
+		}
+	}
+    return -1;
 }
