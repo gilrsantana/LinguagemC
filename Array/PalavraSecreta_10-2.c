4263985ff49636b0f7d42b18f1b1c1e8a5b4115e
Gilmar Santana <53362452+gilrsantana@users.noreply.github.com>
2020-11-12 11:42:53 -0300

    Renaming arrays

--- Array/PalavraSecreta_10-2.c
+++ Array/PalavraSecreta_10-2.c
@@ -7,27 +7,27 @@
 void leCodigo(int array[], int referencia);
 
 int main(void){
-    int cod[PALAVRA] = {-1};
+    int vCod[PALAVRA] = {-1};
     int secreto;
     int i = 0;
 
-    printf("Informe o cÛdigo secreto ou -1 para sair: ");
+    printf("Informe o primeiro c√≥digo secreto ou -1 para sair: ");
     scanf("%i", &secreto);
 
     while(secreto >= 0){
-        cod[i] = secreto;
-        printf("Informe o cÛdigo secreto ou -1 para sair: ");
+        vCod[i] = secreto;
+        printf("Informe o pr√≥ximo c√≥digo secreto ou -1 para sair: ");
         scanf("%i", &secreto);
         i++;
     }
-    leCodigo(cod, i);
+    leCodigo(vCod, i);
     return 0;
 }
 
 void leCodigo(int array[], int referencia){
-    char string[SIZE] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char vString[SIZE] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     int i = 0;
     for(; i <= referencia ; i++){
-        printf("%c", string[array[i]]);
+        printf("%c", vString[array[i]]);
     }
 }
