aff1dde9729eb210efe14cafb3870ea103f24de9
Gilmar Santana <g.r.santana@hotmail.com>
2020-11-24 06:20:04 -0300

    Adding VendaPassagem_ob11-5.c

---
+++ Array/VendaPassagem_ob11-5.c
@@ -0,0 +1,24 @@
+/*
+ * 5.Uma companhia aérea possui 8 tarifas diferentes para seus vôos codificadas de 1 a 8. 
+ * Faça um programa para gerenciar a venda de passagens de um vôo.  
+ * Inicialmente será fornecido o número da tarifa, valor da tarifa e número de lugares 
+ * disponíveis no vôo nesta tarifa. 
+ * O programa deverá permitir que o usuário digite  o código da tarifa e  a quantidade de lugares
+ * desejados e  informar se foi possível realizar a venda (atualizando a quantidade de lugares 
+ * disponíveis). 
+ * 
+ * O programa deve terminar quando o usuário digitar o código 0 (zero) para o código do vôo
+ * 
+ * DESAFIO: Se não for possível, vender todos lugares desejados na tarifa escolhida, mostrar a 
+ * tarifa que tem vaga com preço mais próximo.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	
+	
+	
+	
+	return 0;
+}
